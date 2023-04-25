ea8b652bc341e88022efc8fed142ca2a91d00c7c
eric6166 <ericnguyen6166@gmail.com>
2023-04-25 10:07:52 +0700

    commit

--- spring-reactive-modules/spring-reactive/src/main/java/com/baeldung/reactive/webflux/annotation/EmployeeSpringApplication.java
+++ spring-reactive-modules/spring-reactive/src/main/java/com/baeldung/reactive/webflux/annotation/EmployeeSpringApplication.java
@@ -17,8 +17,8 @@ public class EmployeeSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeSpringApplication.class, args);
 
-        EmployeeWebClient employeeWebClient = new EmployeeWebClient();
-        employeeWebClient.consume();
+//        EmployeeWebClient employeeWebClient = new EmployeeWebClient();
+//        employeeWebClient.consume();
     }
 
 }
